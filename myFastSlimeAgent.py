8f62ef2ef23af91d745431f5289acd0522d689ad
dzheng2019 <55662540+dzheng2019@users.noreply.github.com>
2021-04-20 16:51:42 -0400

    added randomness
    slimeAgent can not rely on some hidden internal randomness if it wants

--- myFastSlimeAgent.py
+++ myFastSlimeAgent.py
@@ -6,7 +6,9 @@ import random
 
 class MyFastModel():
 
-    def __init__(self):
+    def __init__(self, include_randomness = False):
+        self.randomness = include_randomness
+        
         self.dense1 = np.empty((12,13))
         self.dense2 = np.empty((12,13))
         self.dense3 = np.empty((12,13))
@@ -19,38 +21,38 @@ class MyFastModel():
         
         self.param_count = self.l4
         
-        def find_var(layer):
-            dimensions = layer.shape
-            shape = (dimensions[0], dimensions[1])
-            var = np.random.normal(1, .1, (dimensions[1], dimensions[1]))
-            return var;
-        
-        self.variance1 = find_var(self.dense1)
-        self.variance2 = find_var(self.dense2)
-        self.variance3 = find_var(self.dense3)
-        self.variance4 = find_var(self.dense4)
+        self.variance1 = self.find_var(self.dense1, .1)
+        self.variance2 = self.find_var(self.dense2, .1)
+        self.variance3 = self.find_var(self.dense3, .1)
+        self.variance4 = self.find_var(self.dense4, .1)
+    
+    def find_var(self, layer, std):
+        dimensions = layer.shape
+        shape = (dimensions[0], dimensions[1])
+        var = np.random.normal(1, std, (dimensions[1], dimensions[1]))
+        return var;
         
     def predict(self, inputs):
         inputs = np.array([inputs]).T
 
         # hidden
         x1 = np.append(inputs, [[1]], axis=0)
-        z1 = self.dense1 @ self.variance1 @ x1
+        z1 = self.dense1 @ self.variance1 @ x1 if self.randomness else self.dense1 @ x1
         a1 = tanh(z1)
         
         # hidden
         x2 = np.append(a1, [[1]], axis=0)
-        z2 = self.dense2 @ self.variance2 @ x2
+        z2 = self.dense2 @ self.variance2 @ x2 if self.randomness else self.dense2 @ x2
         a2 = tanh(z2)
         
         # hidden
         x3 = np.append(a2, [[1]], axis=0)
-        z3 = self.dense3 @ self.variance3 @ x3
+        z3 = self.dense3 @ self.variance3 @ x3 if self.randomness else self.dense3 @ x3
         a3 = tanh(z3)
         
         # hidde
         x4 = np.append(a3, [[1]], axis=0)
-        z4 = self.dense4 @ self.variance4 @ x4
+        z4 = self.dense4 @ self.variance4 @ x4 if self.randomness else self.dense4 @ x4
         a4 = tanh(z4)
         
         return(a4)
@@ -65,7 +67,12 @@ class MyFastModel():
         self.dense2 = np.array(layer2_params).reshape((12,13))
         self.dense3 = np.array(layer3_params).reshape((12,13))
         self.dense4 = np.array(layer4_params).reshape((3,13))
-
+        
+        if random.random() < .1:
+            self.variance1 = self.find_var(self.dense1, .1)
+            self.variance2 = self.find_var(self.dense2, .1)
+            self.variance3 = self.find_var(self.dense3, .1)
+            self.variance4 = self.find_var(self.dense4, .1)
 
 if __name__ == '__main__':
     print()
--- slimeTourney.py
+++ slimeTourney.py
@@ -22,33 +22,24 @@ population_size = 128
 total_tournaments = 100000
 save_freq = 1000
 
-def mutate(length, mutation_rate, mutation_sigma):
-  # (not used, in case I wanted to do partial mutations)
-  # create an additive mutation vector of some size
-  mask = np.random.randint(int(1/mutation_rate), size=length)
-  mask = 1-np.minimum(mask, 1)
-  noise = np.random.normal(size=length) * mutation_sigma
-  return mask * noise
-
-# Log results
-logdir = "ga_selfplay"
-if not os.path.exists(logdir):
-  os.makedirs(logdir)
-
-
 # Create two instances of a feed forward policy we may need later.
 
 # policy_left = Model(mlp.games['slimevolleylite'])
 # policy_right = Model(mlp.games['slimevolleylite'])
 # param_count = policy_left.param_count
 
-policy_left = ComplexModel()
-policy_right = ComplexModel()
-param_count = policy_left.param_count
+param_count = ComplexModel().param_count
 print("Number of parameters of the neural net policy:", param_count)
 
 # store our population here
 population = np.random.normal(size=(population_size, param_count)) * 0.5 # each row is an agent.
+agents = []
+
+for weights in population:
+    agent = ComplexModel()
+    agent.set_model_params(weights)
+    agents.append(agent)
+    
 winning_streak = [0] * population_size # store the number of wins for this agent (including mutated ones)
 
 # create the gym environment, and seed it
@@ -59,8 +50,8 @@ np.random.seed(random_seed)
 history = []
 for tournament in range(1, total_tournaments+1):
   m, n = np.random.choice(population_size, 2, replace=False)
-  policy_left.set_model_params(population[m])
-  policy_right.set_model_params(population[n])
+  policy_left = agents[m]
+  policy_right= agents[n]
   # the match between the mth and nth member of the population
   if (tournament % 1000 == 0):
     score, length = rollout(env, policy_right, policy_left, render_mode=True)
@@ -71,22 +62,18 @@ for tournament in range(1, total_tournaments+1):
   # if score is positive, it means policy_right won.
   if score == 0: # if the game is tied, add noise to the left agent.
     population[m] += np.random.normal(size=param_count) * 0.1
+    agents[m].set_model_params(population[m])
   if score > 0:
     population[m] = population[n] + np.random.normal(size=param_count) * 0.1
+    agents[m].set_model_params(population[m])
     winning_streak[m] = winning_streak[n]
     winning_streak[n] += 1
   if score < 0:
     population[n] = population[m] + np.random.normal(size=param_count) * 0.1
+    agents[n].set_model_params(population[n])
     winning_streak[n] = winning_streak[m]
     winning_streak[m] += 1
-
-#   if tournament % save_freq == 0:
-#     model_filename = os.path.join(logdir, "ga_"+str(tournament).zfill(8)+".json")
-#     with open(model_filename, 'wt') as out:
-#       record_holder = np.argmax(winning_streak)
-#       record = winning_streak[record_holder]
-#       json.dump([population[record_holder].tolist(), record], out, sort_keys=True, indent=0, separators=(',', ': '))
-
+    
   if (tournament ) % 100 == 0:
     record_holder = np.argmax(winning_streak)
     record = winning_streak[record_holder]
